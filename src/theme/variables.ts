d956da3d15d9a7abf9d941676c283c42aa56ede5
stevenacz <scoailazaa@gmail.com>
2021-03-16 10:43:53 -0500

    variables colors added

---
+++ src/theme/variables.ts
@@ -0,0 +1,18 @@
+export const color = {
+  colorPrimary: '#181b32',
+  // colorPrimary: 'red', // test //
+
+  colorGreyLight1: '#E8EAF0',
+  // colorGreyLight1: 'blue', // test //
+  colorGreyLight2: '#F6F7F9',
+  // colorGreyLight2: 'peru', // test //
+  colorGreyLight3: '#8D8D9D',
+  // colorGreyLight3: 'green', // test //
+  colorGreyLight4: '#DFE1E6',
+  // colorGreyLight4: 'yellow', // test //
+  colorGreyLight5: '#878FB8',
+  // colorGreyLight5: 'purple', // test //
+
+  colorWhite: '#fff',
+  colorBlack: '#000',
+};
