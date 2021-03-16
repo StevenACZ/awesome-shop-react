c98d6445c7412dc962ab7f065c3270607cb3702c
stevenacz <scoailazaa@gmail.com>
2021-03-16 12:00:27 -0500

    Link to product detail added

--- src/components/product-list/product-item/ProductItem.tsx
+++ src/components/product-list/product-item/ProductItem.tsx
@@ -1,24 +1,29 @@
 // React
 import React from 'react';
 
+// React Router
+import { Link } from 'react-router-dom';
+
 // Styles
 import { ProductItemStyled, Top, Bottom } from './Styles';
 
 // Intefaces
 import { Product } from '../../../data/products';
 
-const ProductItem: React.FC<Product> = ({ title, price, imageUrl }) => {
+const ProductItem: React.FC<Product> = ({ id, title, price, imageUrl }) => {
   return (
-    <ProductItemStyled>
-      <Top>
-        <img src={imageUrl} alt={title} />
-      </Top>
+    <Link to={`/products/${id}`}>
+      <ProductItemStyled>
+        <Top>
+          <img src={imageUrl} alt={title} />
+        </Top>
 
-      <Bottom>
-        <p>{title}</p>
-        <span>${price.toFixed(2)}</span>
-      </Bottom>
-    </ProductItemStyled>
+        <Bottom>
+          <p>{title}</p>
+          <span>${price.toFixed(2)}</span>
+        </Bottom>
+      </ProductItemStyled>
+    </Link>
   );
 };
 
