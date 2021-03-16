084ffcdd79b4cdcca1882777cfc6ad9f2979fae9
stevenacz <scoailazaa@gmail.com>
2021-03-16 10:42:51 -0500

    styled added

---
+++ src/components/layout/header/Styles.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+import { color } from '../../../theme/variables';
+
+export const HeaderStyled = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: ${color.colorBlack};
+  height: 72px;
+
+  & > a {
+    text-decoration: none;
+    color: white;
+    padding: 5px 20px;
+    background-color: ${color.colorGreyLight3};
+    cursor: pointer;
+    border: 1px solid transparent;
+
+    &.active {
+      background-color: ${color.colorPrimary};
+      border: 1px solid ${color.colorGreyLight1};
+    }
+  }
+`;
