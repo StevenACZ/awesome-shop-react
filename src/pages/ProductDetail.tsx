2dd1713d0b59cb66bb13e032da1ada37a4e65abb
stevenacz <scoailazaa@gmail.com>
2021-03-16 12:10:47 -0500

    Product Detail get data added

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+// React
+import React, { useEffect, useState } from 'react';
+
+// React Router
+import { useParams } from 'react-router';
+
+// Data & Interface
+import { products, Product } from '../data/products';
 
 interface Props {}
 
 const ProductDetail: React.FC<Props> = () => {
-  return <div>ProductDetail</div>
-}
+  const params = useParams() as { productId: string };
 
-export default ProductDetail
+  const [product, setProduct] = useState<Product>();
+
+  useEffect(() => {
+    const prod = products.find((product) => product.id === params.productId);
+    setProduct(prod);
+  }, [params]);
+
+  console.log(product);
+  return <div>ProductDetail</div>;
+};
+
+export default ProductDetail;
