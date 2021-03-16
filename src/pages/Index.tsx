2900bc691822af08a57af0c837b3c3312bc545fb
stevenacz <scoailazaa@gmail.com>
2021-03-16 11:52:29 -0500

    ProducList added

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+// React
+import React from 'react';
+import ProductList from '../components/product-list/ProductList';
+
+// Data
+import { products } from '../data/products';
 
 interface Props {}
 
 const Index: React.FC<Props> = () => {
-  return <div>Index</div>
-}
+  return <ProductList products={products} />;
+};
 
-export default Index
+export default Index;
