5aa1fa796174c1ad6f87cfaa103d81dc133179e0
stevenacz <scoailazaa@gmail.com>
2021-03-16 10:03:00 -0500

    Router added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { HashRouter } from 'react-router-dom';
 import './App.css';
+import Routes from './routes/Routes';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello!</h1>
+      <HashRouter>
+        <Routes />
+      </HashRouter>
     </div>
   );
 }
