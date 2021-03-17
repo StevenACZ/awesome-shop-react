66591365d53b15d36c161959c0ccf4d3992608e0
stevenacz <scoailazaa@gmail.com>
2021-03-16 21:29:04 -0500

    firebase added

---
+++ src/firebase/config.ts
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export { firebase };
+export const auth = firebase.auth();
